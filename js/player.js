cc771d49758aa9a4c8b81ca90254923cf510b462
Colin Bate <colin@colinbate.com>
2010-01-31 17:57:46 -0400

    A number of visual enhancements including a logo. Gritter works now. Support scripts now load async.

--- js/player.js
+++ js/player.js
@@ -1,4 +1,4 @@
-var bendai = window.bendai || {}
+
 bendai.Characters = function() {
 	
 	var baseCharacter = function(name, max_hp) {
@@ -18,7 +18,7 @@ bendai.Characters = function() {
 		GetNewPlayer: function(name, max_hp, type) {
 			// create a new local player object.
 			return new Player(name, max_hp, type);
-		}
+		},
 		
 		GetSavedPlayer: function(player_id) {
 			// fetch player data from the server.
