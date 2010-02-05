d724512187a9ec920b0add6a8741bc19a5d6b37a
Colin Bate <colin@colinbate.com>
2010-02-05 00:01:15 -0400

    Working on preliminary user support

--- app/bendai.rb
+++ app/bendai.rb
@@ -1,15 +1,27 @@
 require "rubygems"
 require "sinatra"
 require "haml"
+require "mongo_mapper"
+require "json"
+require "classes/user.rb"
 
 set :haml, {:attr_wrapper => '"'}
 
+MongoMapper.database = 'testdb'
 
 get '/' do
   @scripts = Array.new
   @scripts << '/js/extra/jquery.gritter.min.js'
   @scripts << '/js/bendai.js'
   haml :index
+end
+
+get '/game/user/save' do
+  content_type :json
+  if (params['pwd'] == params['repwd']) then
+    
+  end
+  {'success' => false}.to_json
 end
 
     
